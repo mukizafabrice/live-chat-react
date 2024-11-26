e68c84c24a5949e517fcb974c8d69785e5448ef5
Fabrice <111359225+mukizafabrice@users.noreply.github.com>
2024-11-26 20:26:12 +0200

    setup frontend

--- Connectify/frontend/src/App.js
+++ Connectify/frontend/src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
